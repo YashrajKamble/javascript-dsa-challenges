2aab38e73f6e81c7e34d0094637a955780f29f3e
Yashraj_K <yashrajkamble7727@gmail.com>
2025-12-25 11:49:53 +0530

    7th Done

---
+++ 7/7.js
@@ -0,0 +1,25 @@
+function isPalindrome(str) {
+    let cleaned = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== " ") {
+            cleaned += str[i].toLowerCase();
+        }
+    }
+
+    let left = 0;
+    let right = cleaned.length - 1;
+    while (left < right) {
+        if (cleaned[left] !== cleaned[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+console.log(isPalindrome("madam"));       // true
+console.log(isPalindrome("Madam"));       // true
+console.log(isPalindrome("nurses run"));  // true
+console.log(isPalindrome("sir"));         // false
