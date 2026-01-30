942b9d2252293b3ae22c2c539f3fee6c31c75b05
Yashraj_K <yashrajkamble7727@gmail.com>
2026-01-30 12:11:20 +0530

    48th Done

---
+++ 48/48.js
@@ -0,0 +1,19 @@
+// QUESTION 48 — Find the Missing Letter in a Sequence
+
+function findMissingLetter(str) {
+    if (typeof str !== "string" || str.length < 2) {
+        return undefined;
+    }
+
+    for (let i = 0; i < str.length - 1; i++) {
+        let current = str.charCodeAt(i);
+        let next = str.charCodeAt(i + 1);
+
+        if (next - current > 1) {
+            return String.fromCharCode(current + 1)
+        }
+    }
+    return undefined;
+}
+
+console.log(findMissingLetter("abcef"))
